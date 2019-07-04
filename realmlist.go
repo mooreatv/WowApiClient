84f84eda85c692b6af9754efe599a28cf813d201
Moorea Tv <moorea@ymail.com>
2019-07-04 04:36:46 -0700

    Initial working version

---
+++ realmlist.go
@@ -0,0 +1,67 @@
+// Copyright 2019 MooreaTv moorea@ymail.com
+// All Rights Reserved
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"fortio.org/fortio/fhttp"
+	"fortio.org/fortio/log"
+)
+
+var (
+	regionFlag = flag.String("region", "us", "Region to query (us,eu,kr,tw,cn)")
+	localeFlag = flag.String("locale", "en_US", "Locale to use (en_US, es_ES, fr_FR,...)")
+)
+
+func getToken() string {
+	cid := os.Getenv("OAUTH_CID")
+	if len(cid) == 0 {
+		log.Fatalf("Please specify client id as OAUTH_CID env var")
+	}
+	csec := os.Getenv("OAUTH_SEC")
+	if len(csec) == 0 {
+		log.Fatalf("Please specify client secret as OAUTH_SEC env var")
+	}
+	// curl -u {client_id}:{client_secret} -d grant_type=client_credentials https://us.battle.net/oauth/token
+	tokenURL := "https://us.battle.net/oauth/token" // always US even for EU ?
+	log.Infof("Getting token from %s...", tokenURL)
+
+	o := fhttp.HTTPOptions{
+		URL:               tokenURL,
+		DisableFastClient: true,
+		FollowRedirects:   true,
+		UserCredentials:   cid + ":" + csec,
+		ContentType:       "application/x-www-form-urlencoded",
+		Payload:           []byte("grant_type=client_credentials"),
+	}
+	res, data := fhttp.Fetch(&o)
+	log.LogVf("code = %d, data=%s", res, fhttp.DebugSummary(data, 512))
+
+	var kv map[string]interface{}
+
+	if err := json.Unmarshal(data, &kv); err != nil {
+		log.Fatalf("Unable to parse json result %s: %v", fhttp.DebugSummary(data, 80), err)
+	}
+	token := kv["access_token"]
+	log.Infof("Found token to be %v (in %v)", token, kv)
+	return token.(string)
+}
+
+func main() {
+	token := getToken()
+	url := fmt.Sprintf("https://%s.api.blizzard.com/wow/realm/status?locale=%s&access_token=%s", *regionFlag, *localeFlag, token)
+	log.Infof("Using url %s", url)
+	o := fhttp.HTTPOptions{
+		URL:               url,
+		DisableFastClient: true,
+		FollowRedirects:   true,
+	}
+	res, data := fhttp.Fetch(&o)
+	log.Infof("code = %d, data=%s", res, fhttp.DebugSummary(data, 512))
+
+}
